515f87861c37008c0727cbd09e071d7ecd758104
Alexandre FRADET <alex.fradet.1@gmail.com>
2020-02-03 23:57:56 +0100

    feat(note-card): add some style and hide truncating fade when no overflow(KO)

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('truncator', {static: true}) truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText',  {static: true}) bodyText:  ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+    // TODO FIXME : not working
+    // work out if there is a text overflow and if not, hide the truncator
+    const style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    const viewwableHeight = parseInt(style.getPropertyValue("height"), 10);
+    console.log('viewable : ' + viewwableHeight);
+    console.log('scrollHeight' + this.bodyText.nativeElement.scrollHeight);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewwableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
   }
 
 }
