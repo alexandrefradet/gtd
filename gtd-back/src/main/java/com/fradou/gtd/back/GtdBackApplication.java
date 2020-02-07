fc11f97b92a49794fbd0994969d49c4a659f45ad
Alexandre FRADET <alex.fradet.1@gmail.com>
2020-02-07 14:59:02 +0100

    chore(project): implement maven structure and init back module
    - add pom in parent & front module
    - init spring-boot project

---
+++ gtd-back/src/main/java/com/fradou/gtd/back/GtdBackApplication.java
@@ -0,0 +1,13 @@
+package com.fradou.gtd.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtdBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtdBackApplication.class, args);
+	}
+
+}
---
+++ gtd-back/src/test/java/com/fradou/gtd/back/GtdBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fradou.gtd.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtdBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
